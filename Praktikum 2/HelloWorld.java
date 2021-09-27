34211dac2c313e1392e8616c7306d0d7c7618699
AlifFadhlan <aliffadhlanfadhilah@gmail.com>
2021-09-27 18:45:21 +0700

    HelloWorld.java

---
+++ Praktikum 2/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama : Alif Fadhlan Fadhilah");
+		System.out.println("NIM : 6706213063");
+		System.out.println("Kelas : D3IF-45-03");
+		
+	}
+}
